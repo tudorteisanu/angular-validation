f52b0442ad185b1ddc7e7908d3ad3449217c3644
Tudor Teisanu <tudor.teisanu@powerit.dev>
2023-03-28 10:49:52 +0300

    configured tailwind

--- projects/nanoit-angular-modules/src/lib/form-components/form-input/form-input.component.ts
+++ projects/nanoit-angular-modules/src/lib/form-components/form-input/form-input.component.ts
@@ -1,8 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, Input, Optional, Self } from '@angular/core';
+
+import {
+  ControlValueAccessor,
+  NgControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'FormInput',
   templateUrl: './form-input.component.html',
-  styleUrls: ['./form-input.component.css']
 })
-export class FormInputComponent {}
+export class FormInputComponent implements ControlValueAccessor {
+  @Input() icon: string | undefined = undefined;
+  @Input() type: string = 'text';
+  @Input() disabled: boolean = false;
+
+  constructor(@Self() @Optional() private control: NgControl) {
+    if (control) {
+      this.control.valueAccessor = this;
+    }
+  }
+
+  get areMessageShown(): boolean {
+    return !!this.control?.touched || !!this.control?.dirty;
+  }
+
+  get errors(): any {
+    return this.control.errors;
+  }
+
+  get errorMessage(): string {
+    const messages = [];
+
+    if (!this.errors) {
+      return '';
+    }
+
+    if (this.errors.hasOwnProperty('required')) {
+      messages.push('This is required field');
+    }
+
+    if (this.errors.hasOwnProperty('email')) {
+      messages.push('Invalid email format');
+    }
+
+    if (this.errors.hasOwnProperty('minlength')) {
+      messages.push(
+        `Minimal length is ${this.errors.minlength.requiredLength} (${this.errors.minlength.actualLength})`
+      );
+    }
+
+    if (this.errors.hasOwnProperty('maxlength')) {
+      messages.push(
+        `Minimal length is ${this.errors.maxlength.requiredLength} (${this.errors.maxlength.actualLength})`
+      );
+    }
+
+    return messages.join(', ');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  onChange = (value: any) => {};
+
+  onTouched = () => {};
+  writeValue(value: any): void {
+    this.onChange(value);
+  }
+
+  registerOnChange(fn: (rating: number) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+}
